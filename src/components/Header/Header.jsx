2167933c6cb8f8ca62c753a35239734733eaa7d6
Ajinkya243 <ajinkyagund2@gmail.com>
2025-02-08 18:03:20 +0530

    Add toggle in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,13 @@ const Header=()=>{
             <nav className='navbar navbar-expand-lg'>
             {/* <Link className={`${classes["logo-text"]} navbar-brand`} to="/">BlitzGather</Link> */}
             <Link className={`${classes.logoText} navbar-brand`} to="/">BlitzGather</Link>
-            <input type="text" className="ms-auto"  placeholder="Search by title or tag"  />
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#searchItem" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="searchItem">
+    <input type="text" className="ms-auto"  placeholder="Search by title or tag"   />
+    </div>
+           
             </nav>
             <hr />
             </div>    
