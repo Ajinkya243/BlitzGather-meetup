8d1ceb8e6b5402712b31827f1c507b8674e235c1
Ajinkya243 <ajinkyagund2@gmail.com>
2025-02-11 12:29:34 +0530

    Add react query for fetching

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,41 +1,54 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import classes from './Hero.module.css'
-const Hero=()=>{
-    const[data,setData]=useState();
-    useEffect(()=>{
-        fetch("https://blitz-gather-backend.vercel.app/events").then(resp=>resp.json()).then(resp=>setData(resp));
-    },[]);
+import { useQuery } from '@tanstack/react-query';
+import fetchEvent from './FetchEvent';
+import classes from './Hero.module.css';
+
+
+const Hero=({title})=>{
+    
+    const[type,setType]=useState("Both");
+    const {data,isLoading,error}=useQuery({
+        queryKey: ["events",type,title],
+        queryFn:fetchEvent,
+      });
+      
+    console.log("after fetching data is",data);
     return(
         <>
         <div className="container">
-            <nav className="navbar">
+            <div className="d-flex justify-content-between">
             <h2 className=" display-5">Meetup Events</h2>
-            <select name="" id="" className="form-select w-auto">
-                <option value="Both">Select Event Type</option>
+            <div>
+            <label htmlFor="">Select Event Type:</label>
+            <select name="" id="" className="form-select w-auto" onChange={event=>setType(event.target.value)}>
                 <option value="Both">Both</option>
                 <option value="Online Event">Online Event</option>
                 <option value="Offline Event">Offline Event</option>
             </select>
-            </nav>
-
-            <div className="row g-4">
-            {data && data.map(el=>(
+            </div>
+            </div>
+            {isLoading && <h4 className="text-center display-4">Loading.....</h4>}
+            {error && <h4 className="text-center display-4">Error occur while getting data.</h4>}
+            {!isLoading && !error && !data.length && <h4 className="text-center display-4">No such data found.</h4>}
+            <br />
+            <div className="row g-4 justify-content-center">
                 
-                <div className="col-md-4" >
+            {data?.length>0 && data.map(el=>(
+                
+                <div className="col-md-4" key={el._id} >
                     <Link className={classes.link} to={`/events/details/${el._id}`}>
                 <div className={`${classes['img-container']}`}>
                 <img className={`${classes.img}`} src={el.thumbnail} alt="" />
                 <p className={`${classes['event-type']}`}>{el.type}</p>
                 <p>{el.day} {el.date} * {el.startTime} IST</p>
-                <p><strong>{el.title}</strong></p>
+                <p className={classes.p}><strong>{el.title}</strong></p>
                 </div>
                 </Link>
                 </div>
                 
             ))}
             </div>
-
            
         </div>
         </>
